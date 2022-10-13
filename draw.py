5db125a5238eea5a4f09ae61e1e14c54506326b9
PDundh <dundh.parth@gmail.com>
2022-10-13 19:58:50 +0530

    Starting a basic X and zero game for fun

---
+++ draw.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+# input is taken as 1 | 2| 3|
+#                   4 | 5| 6|
+#                   7 | 8| 9|
+#
+# 1 --> X | -1 --> 0 | 0 --> blank
+
+
+def draw(state):
+    for i, box in enumerate(state):
+        
+        if box == 1:
+            print('X', end='\t')
+        elif box == -1:
+            print('O', end='\t')
+        else:
+            print('_', end='\t')
+
+        if (i+1) % 3 == 0:
+            print('\n')
+
+
+draw(np.array([0, 1, -1, 1, 0, 0, 1, -1, 0]))
