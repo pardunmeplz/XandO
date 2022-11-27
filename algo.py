ba818df6667069244b746298abb3c1653bc791e2
pardunmeplz <dundh.parth@gmail.com>
2022-11-27 23:34:39 +0530

    removed numpy

--- algo.py
+++ algo.py
@@ -12,14 +12,13 @@ to return one of the most favourable moves
 
 import typing
 import random as r
-import numpy as np
 
-def win_check(state, player:typing.Literal[0,-1,1])-> bool:
+def win_check(state:typing.List[int], player:typing.Literal[0,-1,1])-> bool:
     '''
     Function checks if passed player has a winning sequence
     in the passed state array.
     '''
-    moves = np.where(state == player)[0]
+    moves = [x for x in state if x == player]
     wins = [(0, 1, 2), (3, 4, 5), (6, 7, 8),
         (0, 3, 6), (1, 4, 7), (2, 5, 8),
         (0, 4, 8), (2, 4, 6)]
@@ -28,12 +27,11 @@ def win_check(state, player:typing.Literal[0,-1,1])-> bool:
             return True
     return False
 
-def allowed(state):
+def allowed(state:typing.List[int])-> typing.List[int]:
     '''
     Returns all moves in a state that are allowed to be played
     '''
-    ans = np.where(state == 0)[0]
-    return ans
+    return [i for i,x in enumerate(state) if x == 0]
 
 
 class AI:
@@ -48,14 +46,14 @@ class AI:
 
     def __init__(self)-> None:
         # on initialization generates most of the state space tree and stores it in huristicMap
-        self.minimax(np.zeros(9, dtype=int))
+        self.minimax([0]*9)
 
-    def minimax(self, state)-> typing.Literal[1,0,-1]:
+    def minimax(self, state:typing.List[int])-> typing.Literal[1,0,-1]:
         '''
         Method impliments minimax game theory analysis on the given board state
         using a backtracking algorithm and returns which player has the advantage in the game.
         '''
-        player:typing.Literal[-1,1] = 1 if state.sum() == 0 else -1 # current player
+        player:typing.Literal[-1,1] = 1 if sum(state) == 0 else -1 # current player
         pending = allowed(state)
 
         if win_check(state,-player):
@@ -71,7 +69,7 @@ class AI:
         drawcheck = -1 # flag to check for draw (drawcheck = 0 in case of draw)
 
         for move in pending:
-            next_state = np.copy(state)
+            next_state = state[:]
             next_state[move] = player  # played a move
 
             output = self.minimax(next_state)  # checked the result
@@ -92,14 +90,14 @@ class AI:
 
     def __call__(self, state) -> int:
 
-        player = 1 if state.sum() == 0 else -1
+        player = 1 if sum(state) == 0 else -1
         moves = allowed(state)
         r.shuffle(moves)
         analyze = []
 
         # check the advantage for each possible move played
         for move in moves:
-            next_state = np.copy(state)
+            next_state = state[:]
             next_state[move] = player
 
             # check if output already memoized
@@ -117,4 +115,4 @@ class AI:
 
 if __name__ == "__main__":
     play = AI()
-    print(play(np.array([0,0,0,1,0,0,0,0,0])))
+    print(play([0]*9))
