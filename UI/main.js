320a57e5c934ab3c648faa52559a1da7eaaff85a
pardunmeplz <dundh.parth@gmail.com>
2022-11-27 23:37:28 +0530

    Added event listener to connect to websocket

--- UI/main.js
+++ UI/main.js
@@ -1,4 +1,19 @@
 var myBoard = new Board()
-window.addEventListener("DOMContentLoaded", () => {
+const websocket = new WebSocket("ws://localhost:8765/")
+window.addEventListener("DOMContentLoaded", async () => {
+
+    websocket.addEventListener('open', () => {
+        console.log('connection established!!')
+        websocket.addEventListener('message', ({ data }) => {
+            console.log('got message ' + data)
+            message = JSON.parse(data)
+            myBoard.turn = message.turn
+            myBoard.gameOver = message.gameOver
+            myBoard.setState(() => message.boardState)
+            myBoard.player = message.player
+        })
+        websocket.send(JSON.stringify({ 'type': 'request' }))
+        console.log('request sent')
+    })
     // initial stuff in here
 })
